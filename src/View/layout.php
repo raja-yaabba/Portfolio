4530f6e45e9ed700001c6eb5ae869b0c8903423c
raja-yaabba <raja.yb@outlook.fr>
2025-02-12 16:48:09 +0100

    resolution probleme css

--- src/View/layout.php
+++ src/View/layout.php
@@ -15,10 +15,10 @@ $auteur = "Raja Y.";
     <meta name="description" content="<?php echo htmlspecialchars($description); ?>" />
     <meta name="author" content="<?php echo htmlspecialchars($auteur); ?>" />
     <meta property="og:image" content="/assets/img/og-image.png" />
-    <link rel="icon" href="/assets/img/logo.png" type="image/png">
+    <link rel="icon" href="http://localhost/portfolio/assets/img/logo.png" type="image/png">
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="http://localhost/portfolio/assets/css/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
